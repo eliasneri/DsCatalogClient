c69e19497d85602cd70f4bcf72b2d4e6328c1f14
eliasneri <eliasneri@hotmail.com>
2021-02-18 12:01:34 -0300

    project created

---
+++ backEnd/src/test/java/com/DsCatalogClient/com/DsCatalogClient/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DsCatalogClient.com.DsCatalogClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
